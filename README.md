cbfa3f848acf4d4ab126c313c5752a332ffae691
Andreas Hibert <20302948+AndreasHibert@users.noreply.github.com>
2024-02-28 13:13:35 +0100

    added some preconfigured streams and modified the readme

--- README.md
+++ README.md
@@ -6,8 +6,21 @@ since youtube-dl is not maintained anymore I changed this script to yt-dlp
 
 I rewrote the script to my needs.
 
-copy the file to `/usr/local/bin` and set a keybind in your WM to `clofi --audio`, and everytime you press your keybind the script will start playing the default or given youtube URL in the backgroud. Press again to kill mpv.
 
 
+I have configred a few default lofi like streams. feel free to modify it to your needs.
+
+example `clofi -hiphop` will run this URL `https://www.youtube.com/watch?v=jfKfPfyJRdk`  or `clofi -a -adhd` to run this URL with audio only `https://www.youtube.com/watch?v=CmMrm4BpQHU`.
+
+to see the whole list just run `clofi -h`
+
+
+
+copy the file to `/usr/local/bin` and set a keybind in your WM to `clofi -a` , and everytime you press your keybind the script will start playing the default or given youtube URL in the backgroud. Press again to kill mpv.
 
 or you can use clofi directly in the CLI if you want to have a pixelated video from the youtube video.
+
+
+
+example:
+![](clofi.gif)
--- clofi.sh
+++ clofi.sh
@@ -12,6 +12,25 @@
 CLEAR='\033[0m'
 RED='\033[0;31m'
 
+declare -A url_map
+url_map["-hiphop"]="https://www.youtube.com/watch?v=jfKfPfyJRdk"
+url_map["-adhd"]="https://www.youtube.com/watch?v=CmMrm4BpQHU"
+url_map["-chill"]="https://www.youtube.com/embed/AzwgsZUcTfM"
+url_map["-focus"]="https://www.youtube.com/watch?v=aepL6kuX4dM"
+url_map["-stress"]="https://www.youtube.com/watch?v=P7nu1o-mG_4"
+url_map["-study"]="https://www.youtube.com/watch?v=3rmWJAQ0Na4"
+url_map["-jazz"]="https://www.youtube.com/watch?v=MOKnTxzU4z0"
+url_map["-study2"]="https://www.youtube.com/watch?v=GjRMW4EcRAE"
+
+
+# Function to select a random video
+select_random_url() {
+    local urls=("${!url_map[@]}") # Retrieve all keys from the array
+    local random_key=${urls[$RANDOM % ${#urls[@]}]} # Select a random key
+    URL=${url_map[$random_key]} # Set URL to the value corresponding to the random key
+}
+
+
 usage() {
   if [ -n "$1" ]; then
     echo -e "${RED}👉 $1${CLEAR}\n"
@@ -21,7 +40,21 @@ usage() {
   Usage: $(basename "$0") [REQUIRED -u url] [options]
     -h, --help     Show this message
     -u, --url      (Required) URL
-    --audio        Audio only
+    -a, --audio    Audio only
+    -r, --random   random stream
+  
+    ------------------------------------------
+  
+  preconfigured streams:
+
+    -hiphop        lofi hip hop radio
+    -adhd          ADHD Relief Music
+    -chill         Summer Chill
+    -focus         Deep Focus Music
+    -stress        Stress and Anxiety Reduction
+    -study         Music for Studying
+    -study2        Ambient Music for Studying
+    -jazz          Warm Jazz Music for Studying
   
   Examples: $(basename "$0") --audio -u "https://www.youtube.com/watch?v=jfKfPfyJRdk"
 EOF
@@ -38,12 +71,20 @@ fi
 while [[ "$#" -gt 0 ]]; do case $1 in
       -h|--help)   usage; exit 0 ;;
       -u|--url)    URL="$2"; shift; shift ;;
-      --audio)     AUDIO=1; shift ;;
-      *)           usage "Unknown parameter passed: $1"; exit 1 ;;
+      -a|--audio)  AUDIO=1; shift ;;
+      -r|--random) select_random_url; shift ;;
+      *)           
+          if [[ ${url_map[$1]+_} ]]; then
+                URL="${url_map[$1]}"
+                shift
+            else
+                usage "Unknown parameter passed: $1"; exit 1
+            fi
+            ;;
 esac; done
 
 if [ -z "$URL" ]; then
-  usage "No URL specified using default URL"
+  echo "No URL specified using default URL"
   URL="https://www.youtube.com/watch?v=jfKfPfyJRdk"
 fi
 
